b2bbf7f2f8af6deac882e7138b6d1e2bb83b5dce
vdrx7 <150599605+vdrx7@users.noreply.github.com>
2023-11-19 01:58:52 +0000

    pc1

---
+++ PC1/Ex_8.py
@@ -0,0 +1,13 @@
+time = input('Indique la hora: ')
+hours, minutes = time.split(":")
+hours = int(hours)
+minutes = int(minutes)
+
+if hours==7 or (hours==8 and minute==0):
+    print('Es hora del desayuno')
+
+if hours==12 or (hours==13 and minute==0):
+    print('Es hora del almuerzo')
+
+if hours==18 or (hours==19 and minute==0):
+    print('Es hora de la cena')
