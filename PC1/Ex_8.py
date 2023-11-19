c5f216b9a603c9e8160b7d09dee7da5e28caf3bd
vdrx7 <150599605+vdrx7@users.noreply.github.com>
2023-11-19 02:05:38 +0000

    pc1

--- PC1/Ex_8.py
+++ PC1/Ex_8.py
@@ -4,10 +4,10 @@ hours = int(hours)
 minutes = int(minutes)
 
 if hours==7 or (hours==8 and minute==0):
-    print('Es hora del desayuno')
+    print('Es hora de tomar desayuno')
 
 if hours==12 or (hours==13 and minute==0):
-    print('Es hora del almuerzo')
+    print('Es hora de almorzar')
 
 if hours==18 or (hours==19 and minute==0):
-    print('Es hora de la cena')
+    print('Es hora de cenar')
